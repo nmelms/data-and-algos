29841807da13fdaf038189948afe6b0a0671ded0
Nick Melms <nick@craftedagency.com>
2024-06-15 16:51:50 -0400

    can construst memoized

--- canConstruct.js
+++ canConstruct.js
@@ -1,17 +1,27 @@
-const str = "enterapotentpot";
-const wordBank = ["a", "p", "ent", "enter", "ot", "o", "t"];
+const str = "eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef";
+const wordBank = [
+  "e",
+  "eee",
+  "eeeee",
+  "eeeeeee",
+  "eeeeeee",
+  "eeeeeeeee",
+  "eeeeeeeeeeeeee",
+];
 
-const canConstruct = (str, wordBank) => {
+const canConstruct = (str, wordBank, memo = {}) => {
+  if (memo[str]) return memo[str];
   if (str === "") return true;
 
   for (let word of wordBank) {
     if (str.startsWith(word)) {
-      console.log(word);
-      if (canConstruct(str.slice(word.length), wordBank)) {
+      if (canConstruct(str.slice(word.length), wordBank, memo)) {
+        memo[str] = true;
         return true;
       }
     }
   }
+  memo[str] = false;
   return false;
 };
 
